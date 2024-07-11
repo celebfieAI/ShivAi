ff9afbf263fca050c65076dac59efc2ed6d178dc
nishantdev365 <nishantk322@gmail.com>
2024-07-11 13:28:43 +0530

    fix bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,45 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider, useDispatch, useSelector } from "react-redux";
-import { store, RootState } from "./stores/store"; // Adjust the path if necessary
+import { store } from "./stores/store";
 import Router from "./router";
-import ScrollToTop from "@/components/Base/ScrollToTop"; // Adjust the path if necessary
+import ScrollToTop from "@/components/Base/ScrollToTop";
 import "./assets/css/app.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Forgot from "./pages/Forgotpassword";
+import Forgot from "./pages/Forgotpassword"
 import { auth } from "./firebase";
 import { loginUser, setLoading } from "./stores/userSlice";
+import { RootState } from "./stores/store"; // Adjust the path accordingly
 
-// Ensure the root is only created once
-let root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-
-const App: React.FC = () => {
+const App = () => {
   const dispatch = useDispatch();
   const user = useSelector((state: RootState) => state.user.user);
-  // const isLoading = useSelector((state: RootState) => state.user.isLoading);
+  const isLoading = useSelector((state: RootState) => state.user.isLoading);
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+    auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         dispatch(
           loginUser({
             uid: authUser.uid,
-            username: authUser.displayName || "", // Add default value to avoid undefined
-            email: authUser.email || "", // Add default value to avoid undefined
+            username: authUser.displayName,
+            email: authUser.email,
           })
         );
       }
       dispatch(setLoading(false));
     });
-    return () => unsubscribe(); // Clean up the subscription on component unmount
   }, [dispatch]);
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="full-page-loader">
-  //       <div className="lds-dual-ring"></div>
-  //     </div>
-  //   );
-  // }
+  if (isLoading) {
+    return (
+      <div className="full-page-loader">
+        <div className="lds-dual-ring"></div>
+      </div>
+    );
+  }
 
   return (
     <BrowserRouter>
@@ -63,7 +60,7 @@ const App: React.FC = () => {
   );
 };
 
-root.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <App />
   </Provider>
